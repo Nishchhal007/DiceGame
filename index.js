72368692bcbe95eed5d7e48db461c73610a55c39
Nishchhal <nishchhal901@gmail.com>
2022-08-05 10:36:35 +0530

    Refresh me added

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ let a = new Date();
 let yr = a.getFullYear();
 document.getElementById("cr").innerHTML = " " + yr;
 
-    
-    var randomNumber1 = Math.floor(Math.random()*6+1);
-    var randomNumber2 = Math.floor(Math.random()*6+1);
-    
-    var leftDiceImage  = document.getElementById("leftDice").setAttribute('src',`images/dice${randomNumber1}.png`);
-    var rightDiceImage  = document.getElementById("rightDice").setAttribute('src',`images/dice${randomNumber2}.png`);
-    
-    if (randomNumber1>randomNumber2) {
-        document.getElementById("result").innerText = "🚩 Player 1 Wins";
-    }
-    
-    else if (randomNumber1<randomNumber2) {
-        document.getElementById("result").innerText = "Player 2 Wins 🚩";
-    }
-    
-    else{
-        document.getElementById("result").innerText = "Match Draw!";
-    
-    }
-    
+        var randomNumber1 = Math.floor(Math.random()*6+1);
+        var randomNumber2 = Math.floor(Math.random()*6+1);
+        
+        var leftDiceImage  = document.getElementById("leftDice").setAttribute('src',`images/dice${randomNumber1}.png`);
+        var rightDiceImage  = document.getElementById("rightDice").setAttribute('src',`images/dice${randomNumber2}.png`);
+        
+        if (randomNumber1>randomNumber2) {
+            document.getElementById("result").innerText = "🚩 Player 1 Wins";
+        }
+        
+        else if (randomNumber1<randomNumber2) {
+            document.getElementById("result").innerText = "Player 2 Wins 🚩";
+        }
+        
+        else{
+            document.getElementById("result").innerText = "Match Draw!";
+        
+        }
+
+        setInterval(() => {
+            document.getElementById("result").innerText = "Refresh Me";
+        }, 1500);
+
 
 // console.log(randomNumber1);
