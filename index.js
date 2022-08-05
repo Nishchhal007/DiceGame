c597152fbcefe261f790c5f6a24fae91e715f55c
Nishchhal <nishchhal901@gmail.com>
2022-08-05 10:23:47 +0530

    JS Added App working

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Copyright Year
 
-
 let a = new Date();
 let yr = a.getFullYear();
 document.getElementById("cr").innerHTML = " " + yr;
+
+    
+    var randomNumber1 = Math.floor(Math.random()*6+1);
+    var randomNumber2 = Math.floor(Math.random()*6+1);
+    
+    var leftDiceImage  = document.getElementById("leftDice").setAttribute('src',`images/dice${randomNumber1}.png`);
+    var rightDiceImage  = document.getElementById("rightDice").setAttribute('src',`images/dice${randomNumber2}.png`);
+    
+    if (randomNumber1>randomNumber2) {
+        document.getElementById("result").innerText = "🚩 Player 1 Wins";
+    }
+    
+    else if (randomNumber1<randomNumber2) {
+        document.getElementById("result").innerText = "Player 2 Wins 🚩";
+    }
+    
+    else{
+        document.getElementById("result").innerText = "Match Draw!";
+    
+    }
+    
+
+// console.log(randomNumber1);
